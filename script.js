31a8613eb9b2a61a963a38674caa2f3432dabed4
aladuuu <55522084+aladuuu@users.noreply.github.com>
2024-05-27 17:28:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const logo = document.getElementById('logo');
+    const text = document.getElementById('construction-text');
 
     gsap.to(logo, {
         duration: 2,
         opacity: 1,
-        ease: "power2.inOut"
+        filter: "blur(0px)",
+        ease: "power2.inOut",
+        onComplete: () => {
+            logo.style.filter = "none";
+        }
+    });
+
+    gsap.to(text, {
+        duration: 2,
+        opacity: 1,
+        filter: "blur(0px)",
+        ease: "power2.inOut",
+        delay: 1,
+        onComplete: () => {
+            text.style.filter = "none";
+        }
     });
 });
