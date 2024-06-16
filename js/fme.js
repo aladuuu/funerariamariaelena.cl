8665bcc28500ea9af8762be9fe625ffda345765a
aladuuu <55522084+aladuuu@users.noreply.github.com>
2024-06-16 08:21:26 -0400

    update

--- js/fme.js
+++ js/fme.js
@@ -227,3 +227,16 @@ const owen = `
     <li>Trámites Registro civil</li>
   </ul>
 `;
+document.querySelectorAll('.velorio-accordion-item button').forEach(button => {
+  button.addEventListener('click', () => {
+      const content = button.nextElementSibling;
+
+      button.classList.toggle('active');
+
+      if (button.classList.contains('active')) {
+          content.style.display = 'block';
+      } else {
+          content.style.display = 'none';
+      }
+  });
+});
